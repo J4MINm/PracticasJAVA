a5c7537a52660c3b6229c22515ac37b4a3f03c28
J4MINlm <114263636+J4MINm@users.noreply.github.com>
2022-10-09 13:17:47 -0500

    Practica1

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+/*
+ *  Firs program with java.
+ *  Author; @EV3TH
+ */
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.print("\nHello World!");
+    }
+}
